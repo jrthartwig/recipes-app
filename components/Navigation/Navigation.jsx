76a5915ed09ba53c357ab7af1cde0eadd2028ebd
jrthartwig <jocelynnhartwig@gmail.com>
2022-02-21 23:11:22 -0500

    feat: styling tabs

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -3,26 +3,33 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CreateRecipe from '../CreateRecipe/CreateRecipe';
 import RecipeListing from '../RecipeListing/RecipeListing';
 import { StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator tabBarStyle={{ backgroundColor: "#3C698B" }} screenOptions={{
+            <Tab.Navigator screenOptions={{
                 tabBarLabelStyle: {
                     fontSize: 18,
-                    color: "white"
+                    color: "white",
+                    fontWeight: "bold"
                 },
                 tabBarStyle: {
-                    backgroundColor: "#3C698B"
+                    backgroundColor: "#3C698B",
+                    height: 110, 
+                    borderColor: "white",
                 },
+                tabBarActiveTintColor: "#d75934",
+                tabBarInactiveTintColor: "#fff",
                 tabBarIconStyle: {
-                    display: 'none'
+                    flex: 1,
+                    justifyContent: 'center'
                 }
             }}>
-                <Tab.Screen name="Create a Recipe" component={CreateRecipe} />
-                <Tab.Screen name="View Recipes" component={RecipeListing} />
+                <Tab.Screen options={{tabBarIcon: ({color, size}) => (<Icon name="pencil" color={color} size={30} />)}} name="Create a Recipe" component={CreateRecipe} />
+                <Tab.Screen options={{tabBarIcon: ({color, size}) => (<Icon name="open-book" color={color} size={30} />)}} name="View Recipes" component={RecipeListing} />
             </Tab.Navigator>
         </NavigationContainer>
     );
